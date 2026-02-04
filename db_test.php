e4f320d29bf38d8d01dcf5c7ccb1b59b613aaf26
Md Sahrior Haider Shovon <165468630+Sahrior@users.noreply.github.com>
2026-02-04 23:33:08 +0600

    mysql connected

---
+++ db_test.php
@@ -0,0 +1,9 @@
+<?php
+$conn = new mysqli("localhost", "root", "");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "MySQL Connected Successfully!";
+?>
