993359dd2d30850c5fe3ab8ff9e115e4c308cd73
Md Sahrior Haider Shovon <165468630+Sahrior@users.noreply.github.com>
2026-02-09 07:15:20 +0600

    MySQL Queries in PHP

--- MySQL_Fall_2024.php
+++ MySQL_Fall_2024.php
@@ -26,7 +26,53 @@ CREATE TABLE IF NOT EXISTS student_final(
     LetterGrade CHAR(1)
 )");
 
+$conn->query("
+INSERT IGNORE INTO student_final
+(StudentID,StudntName, CourseID, CourseTittle,Grade, LetterGrade)
+VALUES
+(1,'karim uddin',101,'Web programming',85,'B'),
+(2,'Rahim Ahmed',101,'Web programming',92,'A'),
+(3,'Jashim Hossain',102,'Project Managemnt',78,'C')
+");
 
+
+echo "<b>Letter Grade Count</b><br>";
+
+$r = $conn->query("
+SELECT LetterGrade, COUNT(*) AS Total
+FROM student_final
+GROUP BY LetterGrade
+");
+
+while($row= $r->fetch_assoc()){
+    echo $row['LetterGrade'].": ".$row['Total']."<br>";
+}
+
+$conn->query("
+UPDATE student_final
+SET LetterGrade = 'C'
+WHERE Grade<75 AND LetterGrade != 'D'
+");
+ 
+$conn->query("
+UPDATE student_final
+SET LetterGrade = 'C'
+WHERE Grade >80 AND Grade+5 <= 90
+");
+
+
+echo "<b>Course popularity</b><br>";
+
+$r = $conn->query("
+SELECT CourseTittle, COUNT(*) AS Total
+FROM student_final
+GROUP BY CourseTittle
+ORDER BY Total DESC
+");
+
+while($row=$r->fetch_assoc()){
+    echo $row['CourseTittle']. ": " .$row['Total']."<br>";
+}
 
 
 
